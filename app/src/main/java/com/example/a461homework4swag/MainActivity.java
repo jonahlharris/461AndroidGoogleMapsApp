32163103f560980ce1df72002680de48807e15ba
jonahlharris <jonahlharris@utexas.edu>
2018-03-27 19:00:19 -0500

    Initial commit

---
+++ app/src/main/java/com/example/a461homework4swag/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a461homework4swag;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
